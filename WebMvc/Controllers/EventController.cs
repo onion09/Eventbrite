c2a381e0e6ef415d86c5e7a5ba768985f335ee89
coo <46950239+onion09@users.noreply.github.com>
2022-08-17 13:49:08 -0700

    Token Added

--- WebMvc/Controllers/EventController.cs
+++ WebMvc/Controllers/EventController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebMvc.Models;
 using WebMvc.Services;
@@ -42,6 +43,12 @@ namespace WebMvc.Controllers
             };
 
             return View(vm);
+        }
+
+        [Authorize]
+        public IActionResult About()
+        {
+            return View();
         }
     }
 }
