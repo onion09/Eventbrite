e3e1a9719cc2145320be08018cc3413cbb98882a
Lidhuang <lidhuang@outlook.com>
2022-08-26 15:38:56 -0700

    OrderApi
    in line with Kal's progress of last Sunday
    Add Models
    Add Data
    Did the migration
    update startup
    update program

---
+++ OrderApi/Data/OrdersContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderApi.Models;
+
+namespace OrderApi.Data
+{
+    public class OrdersContext: DbContext
+    {
+        public OrdersContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
